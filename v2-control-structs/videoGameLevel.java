0cc349bcc945d5c71cd3a27048013a5da8773384
Johan Alexis Contreras Granados <126220172+johancg04@users.noreply.github.com>
2026-06-17 00:28:05 -0500

    Rename class Main to videoGameLevel

--- v2-control-structs/videoGameLevel.java
+++ v2-control-structs/videoGameLevel.java
@@ -1,4 +1,4 @@
-public class Main {
+public class videoGameLevel {
     public static void main(String[] args) {
         for (int nivel = 1; nivel <= 10; nivel++) {
             System.out.println("Nivel " + nivel);
